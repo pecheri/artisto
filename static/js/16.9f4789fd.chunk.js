a2ccddb0abc2b53fcb0c3f92e6902be6eda5c449
Ryo K Inciong <ryo@Ryos-MacBook-Pro.local>
2021-08-29 00:01:57 -0700

    Updates

--- static/js/16.9f4789fd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpartisto=this.webpackJsonpartisto||[]).push([[16],{107:function(t,n,c){"use strict";c.r(n),c.d(n,"default",(function(){return e}));var i=c(8),r=(c(3),c(11)),s=c(4);function e(){var t=Object(i.g)();return Object(s.jsx)("div",{children:Object(s.jsx)("button",{onClick:function(){t.push(r.a)},children:"Start"})})}}}]);
-//# sourceMappingURL=16.9f4789fd.chunk.js.map
